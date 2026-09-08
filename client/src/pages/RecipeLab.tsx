b30f7170d4fced018c86e6927cd8350224a7e018
chinedu15 <smithbabawande@gmail.com>
2026-09-08 19:46:32 +0100

    bread

--- client/src/pages/RecipeLab.tsx
+++ client/src/pages/RecipeLab.tsx
@@ -186,7 +186,7 @@ function breadToCard(bread: Bread): CollectionCard {
     portionLabel: `${grams} g / portion`,
     allergens,
     dietaryTags: [],
-    region: 'nigeria',
+    region: 'bread',
     bread,
   };
 }
@@ -329,7 +329,7 @@ export function RecipeLab() {
     recipes.forEach((r) => {
       if (r.cuisine?.slug) set.add(r.cuisine.slug);
     });
-    set.add('nigerian');
+    set.add('bread');
     return Array.from(set).sort();
   }, [recipes]);
 
