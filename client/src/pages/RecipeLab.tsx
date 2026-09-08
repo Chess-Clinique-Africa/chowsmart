83a89315cc1df0c83e15bb4e38b17892af0d4592
chinedu15 <smithbabawande@gmail.com>
2026-09-08 19:57:26 +0100

    chowsmartbread

--- client/src/pages/RecipeLab.tsx
+++ client/src/pages/RecipeLab.tsx
@@ -329,7 +329,7 @@ export function RecipeLab() {
     recipes.forEach((r) => {
       if (r.cuisine?.slug) set.add(r.cuisine.slug);
     });
-    set.add('bread');
+    set.add('chowsmartbread');
     return Array.from(set).sort();
   }, [recipes]);
 
